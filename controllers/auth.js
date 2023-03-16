dc96a1779be253677dd2c7de86282ab58b6998d0
mazenfteha <mazn34903@gmail.com>
2023-03-16 16:47:30 -0700

    work on register route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,20 @@
+const User =require('../models/User')
+const {StatusCodes} =require('http-status-codes')
+const { BadRequestError } = require('../errors')
+const bcrypt = require('bcryptjs')
+
+
 const register = async (req, res) =>{
-    res.send('register user')
+
+    const {name, email, password} = req.body  //new tempory user object 
+
+    const salt =await bcrypt.genSalt(10);
+    const hashPassword =await bcrypt.hash(password,salt)
+
+    const tempUser = {name,email,password:hashPassword}
+
+    const user = await User.create({ ...tempUser })
+    res.status(StatusCodes.CREATED).json({user})
 }
 
 const login =async (req, res) =>{
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcryptjs')
 const UserSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -18,7 +18,6 @@ const UserSchema = new mongoose.Schema({
         type:String,
         required:[true, 'please provide password'],
         minlength:6,
-        maxlength:12,
     },
 })
 
