c4c102cf18320dc4aaf59d7d068b767c0a21a51e
mazenfteha <mazn34903@gmail.com>
2023-03-15 21:50:22 -0700

    setup our routes (auth & jobs) and connect to db

---
+++ routes/jobs.js
@@ -0,0 +1,14 @@
+const express =require('express')
+const router =express.Router()
+
+const {getAlljobs,
+        getJob,
+        createJob,
+        updateJob,
+        deleteJob
+    } =require('../controllers/jobs')
+
+router.route('/').post(createJob).get(getAlljobs)
+router.route('/:id').get(getJob).patch(updateJob).delete(deleteJob)
+
+module.exports =router
