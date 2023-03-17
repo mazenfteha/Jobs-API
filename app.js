361964a5f851396360b966c583770c0c33dcade3
mazenfteha <mazn34903@gmail.com>
2023-03-17 16:19:32 -0700

    setup authentication middleware so only the request with JWT can access jobs routes

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
-//conectDB
+
 const connectDB =require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
 
 //routers
 const authRouter =require('./routes/auth')
@@ -21,7 +22,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth',authRouter)
-app.use('/api/v1/jobs',jobsRouter)
+app.use('/api/v1/jobs',authenticateUser,jobsRouter)
 
 
 app.use(notFoundMiddleware);
